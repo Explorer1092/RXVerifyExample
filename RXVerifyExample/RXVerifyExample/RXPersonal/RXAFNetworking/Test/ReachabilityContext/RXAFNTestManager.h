ec80af6cfaee704d75fdde3417873ea81a6ceb93
Rush <xzjxylophone@126.com>
2018-11-27 11:41:44 +0800

    add file name

---
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Test/ReachabilityContext/RXAFNTestManager.h
@@ -0,0 +1,13 @@
+//
+//  RXAFNTestManager.h
+//  RXVerifyExample
+//
+//  Created by Rush.D.Xzj on 2018/11/27.
+//  Copyright © 2018 Rush.D.Xzj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RXAFNTestManager : NSObject
+
+@end
---
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Test/ReachabilityContext/RXAFNTestManager.m
@@ -0,0 +1,13 @@
+//
+//  RXAFNTestManager.m
+//  RXVerifyExample
+//
+//  Created by Rush.D.Xzj on 2018/11/27.
+//  Copyright © 2018 Rush.D.Xzj. All rights reserved.
+//
+
+#import "RXAFNTestManager.h"
+
+@implementation RXAFNTestManager
+
+@end
