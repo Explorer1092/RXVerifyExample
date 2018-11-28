7bffa2e91c209a9b56d77824a055dd0b9bdaa751
Rush <xzjxylophone@126.com>
2018-11-28 19:35:32 +0800

    part

--- RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Test/ReachabilityContext/RXAFNTestManager.m
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Test/ReachabilityContext/RXAFNTestManager.m
@@ -8,6 +8,48 @@
 
 #import "RXAFNTestManager.h"
 
+//typedef struct {
+//    CFIndex        version;
+//    void *        __nullable info;
+//    const void    * __nonnull (* __nullable retain)(const void *info);
+//    void        (* __nullable release)(const void *info);
+//    CFStringRef    __nonnull (* __nullable copyDescription)(const void *info);
+//} SCNetworkReachabilityContext;
+
+typedef NS_ENUM(NSInteger, RXAFNStatus) {
+    RXAFNStatus_Value1,
+    RXAFNStatus_Value2
+};
+
+NSString *NSStringFromRXAFNStatus(RXAFNStatus status) {
+    switch (status) {
+        case RXAFNStatus_Value1:
+        return @"RXAFN状态1";
+        case RXAFNStatus_Value2:
+        return @"RXAFN状态2";
+        default:
+        return @"未知";
+    }
+}
+
+typedef void (^RXAFNStatusBlock)(RXAFNStatus status);
+
+
+
+typedef struct {
+    CFIndex version;
+    void * __nullable info;
+    const void    * __nonnull (* __nullable retain)(const void *info);
+    void        (* __nullable release)(const void *info);
+    CFStringRef    __nonnull (* __nullable copyDescription)(const void *info);
+}RXAFNContext;
+
+
+
 @implementation RXAFNTestManager
+
+- (void)startMonitoring {
+    
+}
 
 @end
