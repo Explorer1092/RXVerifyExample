23827e5651547d4b250503de426ea132c5c8651a
Rush <xzjxylophone@126.com>
2018-12-01 17:54:47 +0800

    add

---
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerialization.h
@@ -0,0 +1,13 @@
+//
+//  RXAFURLRequestSerialization.h
+//  RXVerifyExample
+//
+//  Created by Rush.D.Xzj on 2018/12/1.
+//  Copyright © 2018 Rush.D.Xzj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RXAFURLRequestSerialization : NSObject
+
+@end
---
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerialization.m
@@ -0,0 +1,13 @@
+//
+//  RXAFURLRequestSerialization.m
+//  RXVerifyExample
+//
+//  Created by Rush.D.Xzj on 2018/12/1.
+//  Copyright © 2018 Rush.D.Xzj. All rights reserved.
+//
+
+#import "RXAFURLRequestSerialization.h"
+
+@implementation RXAFURLRequestSerialization
+
+@end
