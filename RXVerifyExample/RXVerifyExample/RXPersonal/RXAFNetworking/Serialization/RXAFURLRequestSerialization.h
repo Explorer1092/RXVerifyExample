7a00c3ceea1c140371fcbb17fb235364571dc221
Rush <xzjxylophone@126.com>
2018-12-02 18:54:45 +0800

    add

--- RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerialization.h
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerialization.h
@@ -7,7 +7,50 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <TargetConditionals.h>
+
+#if TARGET_OS_IOS || TARGET_OS_TV
+#import <UIKit/UIKit.h>
+#elif TARGET_OS_WATCH
+#import <WatchKit/WatchKit.h>
+#endif
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+// TODOAFN_M_3_K_1
+/**
+ Returns a percent-escaped string following RFC 3986 for a query string key or value.
+ RFC 3986 states that the following characters are "reserved" characters.
+ - General Delimiters: ":", "#", "[", "]", "@", "?", "/"
+ - Sub-Delimiters: "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="
+ 
+ In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
+ query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
+ should be percent-escaped in the query string.
+ 
+ @param string The string to be percent-escaped.
+ 
+ @return The percent-escaped string.
+ */
+FOUNDATION_EXPORT NSString * RXAFPercentEscapedStringFromString(NSString *string);
+
+
+// TODOAFN_M_3_K_2
+/**
+ A helper method to generate encoded url query parameters for appending to the end of a URL.
+ 
+ @param parameters A dictionary of key/values to be encoded.
+ 
+ @return A url encoded query string
+ */
+FOUNDATION_EXPORT NSString * RXAFQueryStringFromParameters(NSDictionary *parameters);
+
+
+
 
 @interface RXAFURLRequestSerialization : NSObject
 
 @end
+
+NS_ASSUME_NONNULL_END
--- RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerialization.m
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerialization.m
@@ -7,6 +7,52 @@
 //
 
 #import "RXAFURLRequestSerialization.h"
+// TODOAFN_M_3_K_1 https://www.jianshu.com/p/c621baa0b2bd
+// https://blog.csdn.net/u013833031/article/details/78828539
+
+/**
+ Returns a percent-escaped string following RFC 3986 for a query string key or value.
+ RFC 3986 states that the following characters are "reserved" characters.
+ - General Delimiters: ":", "#", "[", "]", "@", "?", "/"
+ - Sub-Delimiters: "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="
+ 
+ In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
+ query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
+ should be percent-escaped in the query string.
+ - parameter string: The string to be percent-escaped.
+ - returns: The percent-escaped string.
+ */
+NSString * RXAFPercentEscapedStringFromString(NSString *string) {
+    static NSString * const kAFCharactersGeneralDelimitersToEncode = @":#[]@"; // does not include "?" or "/" due to RFC 3986 - Section 3.4
+    static NSString * const kAFCharactersSubDelimitersToEncode = @"!$&'()*+,;=";
+    
+    NSMutableCharacterSet * allowedCharacterSet = [[NSCharacterSet URLQueryAllowedCharacterSet] mutableCopy];
+    [allowedCharacterSet removeCharactersInString:[kAFCharactersGeneralDelimitersToEncode stringByAppendingString:kAFCharactersSubDelimitersToEncode]];
+    
+    // FIXME: https://github.com/AFNetworking/AFNetworking/pull/3028
+    // return [string stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
+    
+    static NSUInteger const batchSize = 50;
+    
+    NSUInteger index = 0;
+    NSMutableString *escaped = @"".mutableCopy;
+    
+    while (index < string.length) {
+        NSUInteger length = MIN(string.length - index, batchSize);
+        NSRange range = NSMakeRange(index, length);
+        
+        // To avoid breaking up character sequences such as 👴🏻👮🏽
+        range = [string rangeOfComposedCharacterSequencesForRange:range];
+        
+        NSString *substring = [string substringWithRange:range];
+        NSString *encoded = [substring stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
+        [escaped appendString:encoded];
+        
+        index += range.length;
+    }
+    
+    return escaped;
+}
 
 @implementation RXAFURLRequestSerialization
 
