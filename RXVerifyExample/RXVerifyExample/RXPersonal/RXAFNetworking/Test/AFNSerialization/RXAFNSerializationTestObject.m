2ced7d52ffc3bcd5c039b7b189a9234f31f25bbc
Rush <xzjxylophone@126.com>
2018-12-05 19:39:33 +0800

    add

--- RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Test/AFNSerialization/RXAFNSerializationTestObject.m
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Test/AFNSerialization/RXAFNSerializationTestObject.m
@@ -31,5 +31,18 @@
                            };
     NSString *v1 = RXAFQueryStringFromParameters(dic1);
     NSLog(@"v1:%@", v1);
+    
+    NSArray *ary1 = RXAFQueryStringPairsFromKeyAndValue(nil, dic1);
+    NSLog(@"ary1:%@", ary1);
+    
+    // 当 value是 array/set/其他  的时候, key是nil是有问题的
+    // 只有当value 是dic的时候,key才能为nil
+    ary1 = RXAFQueryStringPairsFromKeyAndValue(nil, @[@(1), @"abc", @(3333)]);
+    NSLog(@"ary1:%@", ary1);
+    
+    // key = @"kkkk[abc]"
+    // value = @"abc"
+    ary1 = RXAFQueryStringPairsFromKeyAndValue(@"kkkk", dic1);
+    NSLog(@"ary1:%@", ary1);
 }
 @end
