791afb2ee864f624185520e92f0941584bdd2b4f
Rush <xzjxylophone@126.com>
2018-12-11 20:23:59 +0800

    add

--- RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerialization.m
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerialization.m
@@ -13,52 +13,6 @@
 
 #pragma mark - RXAFQueryStringFromParameters
 
-FOUNDATION_EXPORT NSArray * RXAFQueryStringPairsFromDictionary(NSDictionary *dictionary);
-FOUNDATION_EXPORT NSArray * RXAFQueryStringPairsFromKeyAndValue(NSString *key, id value);
-
-NSString * RXAFQueryStringFromParameters(NSDictionary *parameters) {
-    NSMutableArray *mutablePairs = [NSMutableArray array];
-    for (RXAFQueryStringPair *pair in RXAFQueryStringPairsFromDictionary(parameters)) {
-        [mutablePairs addObject:[pair URLEncodedStringValue]];
-    }
-    
-    return [mutablePairs componentsJoinedByString:@"&"];
-}
-
-NSArray * RXAFQueryStringPairsFromDictionary(NSDictionary *dictionary) {
-    return RXAFQueryStringPairsFromKeyAndValue(nil, dictionary);
-}
-
-NSArray * RXAFQueryStringPairsFromKeyAndValue(NSString *key, id value) {
-    NSMutableArray *mutableQueryStringComponents = [NSMutableArray array];
-    
-    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"description" ascending:YES selector:@selector(compare:)];
-    
-    if ([value isKindOfClass:[NSDictionary class]]) {
-        NSDictionary *dictionary = value;
-        // Sort dictionary keys to ensure consistent ordering in query string, which is important when deserializing potentially ambiguous sequences, such as an array of dictionaries
-        for (id nestedKey in [dictionary.allKeys sortedArrayUsingDescriptors:@[ sortDescriptor ]]) {
-            id nestedValue = dictionary[nestedKey];
-            if (nestedValue) {
-                [mutableQueryStringComponents addObjectsFromArray:RXAFQueryStringPairsFromKeyAndValue((key ? [NSString stringWithFormat:@"%@[%@]", key, nestedKey] : nestedKey), nestedValue)];
-            }
-        }
-    } else if ([value isKindOfClass:[NSArray class]]) {
-        NSArray *array = value;
-        for (id nestedValue in array) {
-            [mutableQueryStringComponents addObjectsFromArray:RXAFQueryStringPairsFromKeyAndValue([NSString stringWithFormat:@"%@[]", key], nestedValue)];
-        }
-    } else if ([value isKindOfClass:[NSSet class]]) {
-        NSSet *set = value;
-        for (id obj in [set sortedArrayUsingDescriptors:@[ sortDescriptor ]]) {
-            [mutableQueryStringComponents addObjectsFromArray:RXAFQueryStringPairsFromKeyAndValue(key, obj)];
-        }
-    } else {
-        [mutableQueryStringComponents addObject:[[RXAFQueryStringPair alloc] initWithField:key value:value]];
-    }
-    
-    return mutableQueryStringComponents;
-}
 
 @implementation RXAFURLRequestSerialization
 
--- RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerializationDefine.m
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/Serialization/RXAFURLRequestSerializationDefine.m
@@ -8,7 +8,54 @@
 
 #import "RXAFURLRequestSerializationDefine.h"
 
+#import "RXAFQueryStringPair.h"
 
+FOUNDATION_EXPORT NSArray * RXAFQueryStringPairsFromDictionary(NSDictionary *dictionary);
+FOUNDATION_EXPORT NSArray * RXAFQueryStringPairsFromKeyAndValue(NSString *key, id value);
+
+NSString * RXAFQueryStringFromParameters(NSDictionary *parameters) {
+    NSMutableArray *mutablePairs = [NSMutableArray array];
+    for (RXAFQueryStringPair *pair in RXAFQueryStringPairsFromDictionary(parameters)) {
+        [mutablePairs addObject:[pair URLEncodedStringValue]];
+    }
+    
+    return [mutablePairs componentsJoinedByString:@"&"];
+}
+
+NSArray * RXAFQueryStringPairsFromDictionary(NSDictionary *dictionary) {
+    return RXAFQueryStringPairsFromKeyAndValue(nil, dictionary);
+}
+
+NSArray * RXAFQueryStringPairsFromKeyAndValue(NSString *key, id value) {
+    NSMutableArray *mutableQueryStringComponents = [NSMutableArray array];
+    
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"description" ascending:YES selector:@selector(compare:)];
+    
+    if ([value isKindOfClass:[NSDictionary class]]) {
+        NSDictionary *dictionary = value;
+        // Sort dictionary keys to ensure consistent ordering in query string, which is important when deserializing potentially ambiguous sequences, such as an array of dictionaries
+        for (id nestedKey in [dictionary.allKeys sortedArrayUsingDescriptors:@[ sortDescriptor ]]) {
+            id nestedValue = dictionary[nestedKey];
+            if (nestedValue) {
+                [mutableQueryStringComponents addObjectsFromArray:RXAFQueryStringPairsFromKeyAndValue((key ? [NSString stringWithFormat:@"%@[%@]", key, nestedKey] : nestedKey), nestedValue)];
+            }
+        }
+    } else if ([value isKindOfClass:[NSArray class]]) {
+        NSArray *array = value;
+        for (id nestedValue in array) {
+            [mutableQueryStringComponents addObjectsFromArray:RXAFQueryStringPairsFromKeyAndValue([NSString stringWithFormat:@"%@[]", key], nestedValue)];
+        }
+    } else if ([value isKindOfClass:[NSSet class]]) {
+        NSSet *set = value;
+        for (id obj in [set sortedArrayUsingDescriptors:@[ sortDescriptor ]]) {
+            [mutableQueryStringComponents addObjectsFromArray:RXAFQueryStringPairsFromKeyAndValue(key, obj)];
+        }
+    } else {
+        [mutableQueryStringComponents addObject:[[RXAFQueryStringPair alloc] initWithField:key value:value]];
+    }
+    
+    return mutableQueryStringComponents;
+}
 NSString * const RXAFURLRequestSerializationErrorDomain = @"com.alamofire.error.serialization.request";
 NSString * const RXAFNetworkingOperationFailingURLRequestErrorKey = @"com.alamofire.serialization.request.error.response";
 
@@ -61,4 +108,8 @@ NSString * RXAFPercentEscapedStringFromString(NSString *string) {
     
     return escaped;
 }
+
+NSUInteger const kRXAFUploadStream3GSuggestedPacketSize = 1024 * 16;
+NSTimeInterval const kRXAFUploadStream3GSuggestedDelay = 0.2;
+
 
