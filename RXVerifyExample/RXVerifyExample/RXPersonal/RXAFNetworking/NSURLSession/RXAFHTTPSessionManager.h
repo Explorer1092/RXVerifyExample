2c9181cee8c94bbb47c1509cd91c73bddaec2b4e
Rush <xzjxylophone@126.com>
2018-12-24 23:09:39 +0800

    add HTTP Session Manager

---
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAFNetworking/NSURLSession/RXAFHTTPSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  RXAFHTTPSessionManager.h
+//  RXVerifyExample
+//
+//  Created by Rush.D.Xzj on 2018/12/24.
+//  Copyright © 2018 Rush.D.Xzj. All rights reserved.
+//
+
+#import "RXAFURLSessionManager.h"
+
+@interface RXAFHTTPSessionManager : RXAFURLSessionManager
+
+@end
