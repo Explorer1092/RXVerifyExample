19d7c5327f8719364c52e6805d66abb7dd2b11b7
Rush <xzjxylophone@126.com>
2018-12-31 17:30:20 +0800

    add weather api

---
+++ RXVerifyExample/RXVerifyExample/Third/Api/WeatherApi/RXCountryWeatherApi.h
@@ -0,0 +1,13 @@
+//
+//  RXCountryWeatherApi.h
+//  RXVerifyExample
+//
+//  Created by Rush.D.Xzj on 2018/12/31.
+//  Copyright © 2018 Rush.D.Xzj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+// https://www.cnblogs.com/wangjingblogs/p/3192953.html
+@interface RXCountryWeatherApi : NSObject
+
+@end
---
+++ RXVerifyExample/RXVerifyExample/Third/Api/WeatherApi/RXCountryWeatherApi.m
@@ -0,0 +1,13 @@
+//
+//  RXCountryWeatherApi.m
+//  RXVerifyExample
+//
+//  Created by Rush.D.Xzj on 2018/12/31.
+//  Copyright © 2018 Rush.D.Xzj. All rights reserved.
+//
+
+#import "RXCountryWeatherApi.h"
+
+@implementation RXCountryWeatherApi
+
+@end
