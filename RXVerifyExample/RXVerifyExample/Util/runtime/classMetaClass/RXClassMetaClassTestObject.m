c32d14a40770a0eb985ffc8686defc1ad15d3971
Rush <xzjxylophone@126.com>
2019-01-07 16:49:58 +0800

    runtime

--- RXVerifyExample/RXVerifyExample/Util/runtime/classMetaClass/RXClassMetaClassTestObject.m
+++ RXVerifyExample/RXVerifyExample/Util/runtime/classMetaClass/RXClassMetaClassTestObject.m
@@ -11,13 +11,26 @@
 #import "RXClassMetaClassOverrideObject.h"
 #import "RXRuntimeUtil.h"
 #import <objc/runtime.h>
+#import "RXClassMetaClassResponseObject.h"
 @implementation RXClassMetaClassTestObject
 - (void)mainTest
 {
-    [self _test];
+    [self _test_class_same];
+//    [self _test];
 //    [self _test2];
 //    [self _test_override];
+    
+//    [self _test_response];
 }
+
+- (void)_test_class_same
+{
+    Class cls1 = [RXClassMetaClassObject class];
+    Class cls2 = [[RXClassMetaClassObject new] class];
+    BOOL value = cls1 == cls2;
+    NSLog(@"value:%zd", value);
+}
+
 
 - (void)_test
 {
@@ -34,6 +47,16 @@
     
     [RXClassMetaClassObject print_same_name];
     [[RXClassMetaClassObject new] print_same_name];
+}
+
+- (void)_test_response
+{
+    BOOL value1 = [RXClassMetaClassResponseObject respondsToSelector:@selector(print_test_response)];
+    
+    RXClassMetaClassResponseObject *object = [RXClassMetaClassResponseObject new];
+    BOOL value2 = [object respondsToSelector:@selector(print_test_response)];
+    
+    NSLog(@"value1:%zd, value2:%zd", value1, value2);
 }
 - (void)_test_override
 {
