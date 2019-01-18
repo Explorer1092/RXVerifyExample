de10a3c06b727cc9237e352e6f32422c120980d8
Rush <xzjxylophone@126.com>
2019-01-18 11:22:20 +0800

    add doc and update

--- RXVerifyExample/RXVerifyExample/RXPersonal/RXBlock/RXBlockChangeOverTimeObject.m
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXBlock/RXBlockChangeOverTimeObject.m
@@ -9,14 +9,12 @@
 #import "RXBlockChangeOverTimeObject.h"
 #import "RXPrintUtil.h"
 
-static NSInteger globalVariable = 1;
+NSInteger globalVariable = 1;
 
 @interface RXBlockChangeOverTimeObject()
 @property (nonatomic, copy) NSInteger(^block)(NSInteger);
-
 @property (nonatomic, assign) NSInteger instanceVariable;
 @end
-
 
 
 
@@ -25,47 +23,67 @@ static NSInteger globalVariable = 1;
 - (void)test
 {
     NSLog(@"--------RXBlockChangeOverTimeObject--------");
+    // 定义几种类型的变量
     self.instanceVariable = 10;
     NSInteger localVariable = 100;
+    static NSInteger staticLocalVariable = 99;
     __block NSInteger blockVariable = 1000;
     
+    // 定义分别使用类型变量的block
     NSInteger (^block_noVariable)(NSInteger) = ^(NSInteger m) {
         return m;
     };
-    [RXPrintUtil printBlockWithPrefix:@"block_noVariable" block:block_noVariable];
-    self.block = block_noVariable;
-    [RXPrintUtil printBlockWithPrefix:@"self block_noVariable" block:self.block];
-    
     NSInteger (^block_globalVariable)(NSInteger) = ^(NSInteger m) {
         return m + globalVariable;
     };
-    [RXPrintUtil printBlockWithPrefix:@"block_globalVariable" block:block_globalVariable];
-    self.block = block_globalVariable;
-    [RXPrintUtil printBlockWithPrefix:@"self block_globalVariable" block:self.block];
-    
     __weak typeof(self) weakSelf = self;
     NSInteger (^block_instanceVariable)(NSInteger) = ^(NSInteger m) {
         return m + weakSelf.instanceVariable;
     };
-    [RXPrintUtil printBlockWithPrefix:@"block_instanceVariable" block:block_instanceVariable];
-    self.block = block_instanceVariable;
-    [RXPrintUtil printBlockWithPrefix:@"self block_instanceVariable" block:self.block];
-    
     NSInteger (^block_localVariable)(NSInteger) = ^(NSInteger m) {
         return m + localVariable;
     };
-    [RXPrintUtil printBlockWithPrefix:@"block_localVariable" block:block_localVariable];
-    self.block = block_localVariable;
-    [RXPrintUtil printBlockWithPrefix:@"self block_localVariable" block:self.block];
-    
-    
+    NSInteger (^block_staticLocalVariable)(NSInteger) = ^(NSInteger m) {
+        return m + staticLocalVariable;
+    };
     NSInteger (^block_blockVariable)(NSInteger) = ^(NSInteger m) {
         blockVariable++;
         return m + blockVariable;
     };
+    NSInteger (^block_composedVarialbe)(NSInteger) = ^(NSInteger m) {
+        return m + localVariable + staticLocalVariable;
+    };
+    
+    // 输出定义好的block变量
+    [RXPrintUtil printBlockWithPrefix:@"block_noVariable" block:block_noVariable];
+    [RXPrintUtil printBlockWithPrefix:@"block_globalVariable" block:block_globalVariable];
+    [RXPrintUtil printBlockWithPrefix:@"block_instanceVariable" block:block_instanceVariable];
+    [RXPrintUtil printBlockWithPrefix:@"block_localVariable" block:block_localVariable];
+    [RXPrintUtil printBlockWithPrefix:@"block_staticLocalVariable" block:block_staticLocalVariable];
     [RXPrintUtil printBlockWithPrefix:@"block_blockVariable" block:block_blockVariable];
+    [RXPrintUtil printBlockWithPrefix:@"block_composedVarialbe" block:block_composedVarialbe];
+    
+    // 分别设置block
+    self.block = block_noVariable;
+    [RXPrintUtil printBlockWithPrefix:@"self block_noVariable" block:self.block];
+    
+    self.block = block_globalVariable;
+    [RXPrintUtil printBlockWithPrefix:@"self block_globalVariable" block:self.block];
+    
+    self.block = block_instanceVariable;
+    [RXPrintUtil printBlockWithPrefix:@"self block_instanceVariable" block:self.block];
+    
+    self.block = block_localVariable;
+    [RXPrintUtil printBlockWithPrefix:@"self block_localVariable" block:self.block];
+    
+    self.block = block_staticLocalVariable;
+    [RXPrintUtil printBlockWithPrefix:@"self block_staticLocalVariable" block:self.block];
+    
     self.block = block_blockVariable;
     [RXPrintUtil printBlockWithPrefix:@"self block_blockVariable" block:self.block];
+    
+    self.block = block_composedVarialbe;
+    [RXPrintUtil printBlockWithPrefix:@"self block_composedVarialbe" block:self.block];
 }
 - (void)dealloc
 {
