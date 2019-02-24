30d884c47e94e88f6db61f21d81baa996ce1a615
Rush <xzjxylophone@126.com>
2019-02-24 10:40:09 +0800

    update

--- RXVerifyExample/RXVerifyExample/RXPersonal/RXAnimation/RXAnimationViewController.m
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAnimation/RXAnimationViewController.m
@@ -10,7 +10,16 @@
 #import "RXAnimationTestObject.h"
 @interface RXAnimationViewController ()
 
+
+
+@property (nonatomic, strong) UIView *parentView;
+
 @property (nonatomic, strong) UIView *animationView;
+@property (nonatomic, strong) UIView *lineView;
+@property (nonatomic, strong) UIView *line2View;
+@property (nonatomic, strong) UIView *line3View;
+@property (nonatomic, strong) UIView *line4View;
+@property (nonatomic, strong) UIView *line5View;
 
 @end
 
@@ -22,12 +31,52 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    self.parentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 1000)];
+    self.parentView.backgroundColor = [UIColor whiteColor];
     
-    self.animationView = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 200, 200)];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    
+    
+    CGFloat startY = 100;
+    CGFloat viewWidth = 200;
+
+    self.animationView = [[UIView alloc] initWithFrame:CGRectMake(100, startY, viewWidth, viewWidth)];
     self.animationView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:self.animationView];
     
-    [self test];
+    self.lineView = [[UIView alloc] initWithFrame:CGRectMake(0, startY + viewWidth / 2.0f, width, 1)];
+    self.lineView.backgroundColor = [UIColor redColor];
+    
+    self.line2View = [[UIView alloc] initWithFrame:CGRectMake(0, startY + viewWidth, width, 1)];
+    self.line2View.backgroundColor = [UIColor redColor];
+    
+    
+    self.line3View = [[UIView alloc] initWithFrame:CGRectMake(0, startY, width, 1)];
+    self.line3View.backgroundColor = [UIColor redColor];
+    
+    
+    
+    
+    [self.view addSubview:self.parentView];
+    
+    [self.parentView addSubview:self.animationView];
+    [self.parentView addSubview:self.lineView];
+    [self.parentView addSubview:self.line2View];
+    [self.parentView addSubview:self.line3View];
+    [self.parentView addSubview:self.line4View];
+    [self.parentView addSubview:self.line5View];
+    
+    
+    
+//    [self test];
+    
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+//        // 坐标也是不正确的
+//        [self test];
+//    });
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
--- RXVerifyExample/RXVerifyExample/RXPersonal/RXAnimation/RXBasicAnimationTestObject.m
+++ RXVerifyExample/RXVerifyExample/RXPersonal/RXAnimation/RXBasicAnimationTestObject.m
@@ -12,14 +12,17 @@
 
 - (void)testInView:(UIView *)view {
     CABasicAnimation *moveAnimation = [CABasicAnimation animationWithKeyPath:@"position.y"];
-    moveAnimation.duration = 0;// 动画持续时间,当等于0的时候,好像是0.25, 当小于0.1大于0的时候好像是0.1
+    moveAnimation.duration = 1;// 动画持续时间,当等于0的时候,好像是0.25, 当小于0.1大于0的时候好像是0.1
+    moveAnimation.speed = 1;
     //动画起始值和终止值的设置
-    moveAnimation.fromValue = @(view.center.x);
-    moveAnimation.toValue = @(view.center.x+100);
+    moveAnimation.fromValue = @(100);
+    moveAnimation.toValue = @(300);
     moveAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
-//    moveAnimation.repeatCount = 10;
-    moveAnimation.repeatDuration = 1;
+//    moveAnimation.repeatCount = 1;
+//    moveAnimation.repeatDuration = 3;
     moveAnimation.removedOnCompletion = NO;
+//    moveAnimation.autoreverses = YES;
+//    moveAnimation.timeOffset = 0.5;
     moveAnimation.fillMode = kCAFillModeForwards;
     [view.layer addAnimation:moveAnimation forKey:@"abc"];
 }
