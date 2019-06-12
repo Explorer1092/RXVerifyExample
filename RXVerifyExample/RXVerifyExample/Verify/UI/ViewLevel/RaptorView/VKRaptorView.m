2203fdbe60703e5e3604925727106c2f8b81755d
Rush.D.xzj <xzjxylophone@gmail.com>
2019-06-12 10:38:57 +0800

    update

--- RXVerifyExample/RXVerifyExample/Verify/UI/ViewLevel/RaptorView/VKRaptorView.m
+++ RXVerifyExample/RXVerifyExample/Verify/UI/ViewLevel/RaptorView/VKRaptorView.m
@@ -13,24 +13,37 @@
 #import "VKGuideLevel.h"
 #import "VKAnimationLevel.h"
 #import "VKVideoLevel.h"
+
+
+#import "VKBgLevelView.h"
+#import "VKNormalLevelView.h"
+
 @interface VKRaptorView() <VKNormalLevelDelegate>
 @property (nonatomic, strong) VKBGLevel *bgLevel;
 @property (nonatomic, strong) VKVideoLevel *videoLevel;
 @property (nonatomic, strong) VKNormalLevel *normalLevel;
 @property (nonatomic, strong) VKGuideLevel *guideLevel;
 @property (nonatomic, strong) VKAnimationLevel *animationLevel;
+
+@property (nonatomic, strong) VKBgLevelView *bgLevelView;
+@property (nonatomic, strong) VKNormalLevelView *normalLevelView;
+
+
 @end
 
 @implementation VKRaptorView
 
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-        self.bgLevel = [[VKBGLevel alloc] initWithSuperview:self];
-        self.videoLevel = [[VKVideoLevel alloc] initWithSuperview:self];
-        self.normalLevel = [[VKNormalLevel alloc] initWithSuperview:self];
-        self.normalLevel.delegate = self;
-        self.animationLevel = [[VKAnimationLevel alloc] initWithSuperview:self];
-        self.guideLevel = [[VKGuideLevel alloc] initWithSuperview:self];
+//        self.bgLevel = [[VKBGLevel alloc] initWithSuperview:self];
+//        self.videoLevel = [[VKVideoLevel alloc] initWithSuperview:self];
+//        self.normalLevel = [[VKNormalLevel alloc] initWithSuperview:self];
+//        self.normalLevel.delegate = self;
+//        self.animationLevel = [[VKAnimationLevel alloc] initWithSuperview:self];
+//        self.guideLevel = [[VKGuideLevel alloc] initWithSuperview:self];
+        
+        self.bgLevelView = [[VKBgLevelView alloc] initWithSuperview:self];
+        self.normalLevelView = [[VKNormalLevelView alloc] initWithSuperview:self];
     }
     return self;
 }
