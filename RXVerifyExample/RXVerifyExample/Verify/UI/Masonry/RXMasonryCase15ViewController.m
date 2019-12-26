1b22bfa8b9e6dc24d50d321cafdae34b1726d1d8
Rush.D.xzj <xzjxylophone@gmail.com>
2019-12-26 11:33:19 +0800

    add 15

--- RXVerifyExample/RXVerifyExample/Verify/UI/Masonry/RXMasonryCase15ViewController.m
+++ RXVerifyExample/RXVerifyExample/Verify/UI/Masonry/RXMasonryCase15ViewController.m
@@ -7,26 +7,59 @@
 //
 
 #import "RXMasonryCase15ViewController.h"
+#import <Masonry/Masonry.h>
 
 @interface RXMasonryCase15ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *leftLabels;
 
+@property (nonatomic, strong) UILabel *greenLabel;
 @end
 
 @implementation RXMasonryCase15ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    _greenLabel = [UILabel new];
+    _greenLabel.backgroundColor = [UIColor greenColor];
+    _greenLabel.text = @"Green Label";
+    [_containerView addSubview:_greenLabel];
+    
+    // 内容优先显示
+    [_greenLabel setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+    
+    [_greenLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        // 垂直居中
+        make.centerY.equalTo(_containerView);
+        
+        // 右边不超出
+        make.right.lessThanOrEqualTo(_containerView);
+        
+        // 左边大于等于父View宽度的1/3，注意不是width属性
+        make.left.greaterThanOrEqualTo(_containerView.mas_right).multipliedBy((CGFloat)(1.0f / 3.0f));
+        
+        // 左边大于所有左边label的右边+8
+        for (UILabel *label in _leftLabels) {
+            make.left.greaterThanOrEqualTo(label.mas_right).offset(8);
+        }
+    }];
 }
+- (NSString *)getText:(NSString *)text withRepeat:(int)repeat {
+    NSMutableString *tmpText = [NSMutableString new];
 
-/*
-#pragma mark - Navigation
+    for (int i = 0; i < repeat; i++) {
+        [tmpText appendString:text];
+    }
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    return tmpText;
 }
-*/
+#pragma mark - Action
+
+- (IBAction)onTapStepper:(UIStepper *)sender {
+    UILabel *label = _leftLabels[sender.tag];
+    label.text = [self getText:@"label" withRepeat:(int)sender.value];
+}
 
 @end
